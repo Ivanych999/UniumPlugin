70b2a202d7d16d90df0c02748c6eca61c62d1ef8
Ivan Medvedev <medvedev.ivan@mail.ru>
2015-12-18 00:05:44 +0300

    Labeling, UI
    Set max scale for labels to 100k
    Add controls to files tab

--- mcqp_unium.py
+++ mcqp_unium.py
@@ -87,7 +87,10 @@ class UniumPlugin:
         self.src_info = {}
         self.selected_id = u''
         self.mercator = QgsCoordinateReferenceSystem()
-        self.mercator.createFromProj4(u'+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs')
+        mercatorWKT = u'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]'
+        mercatorProj4 = u'+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs'
+        #self.mercator.createFromProj4()
+        self.mercator.createFromWkt(mercatorWKT)
 
         # default configuration
         self.default_config = """{"files_folder": "",
@@ -831,6 +834,8 @@ class UniumPlugin:
             canvas.setMapUnits(0)
             canvas.refresh()
             self.set_style_to_lyrs()
+            renderer.setDestinationCrs(self.mercator)
+            canvas.refresh()
             QgsMessageLog.logMessage(u'Слои созданы', level=QgsMessageLog.INFO)
             self.dockwidget.sasprogressBar.setValue(100)
             qApp.processEvents()
